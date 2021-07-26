15623063c3ba20994cb4d39cac2fb59bda922b67
wildmental <wildmental@axiasoft.co.kr>
2021-07-26 20:11:45 +0900

    skipList eps (epsilon) 동작 실험 추가

---
+++ workflows/flow_funcs.go
@@ -0,0 +1,103 @@
+package workflows
+
+import (
+	"bufio"
+	"fmt"
+	"github.com/MauriceGit/skiplist"
+	"io"
+	"math/bits"
+	"math/rand"
+	"os"
+	"strconv"
+	"strings"
+	"time"
+)
+
+type IntElement int
+
+func (e IntElement) ExtractKey() float64 {
+	return float64(e)
+}
+func (e IntElement) String() string {
+	return fmt.Sprintf("%03d", e)
+}
+
+type Element struct {
+	key   float64
+	value string
+}
+
+// Implement the interface used in skipList
+
+func (e Element) ExtractKey() float64 {
+	return e.key
+}
+func (e Element) String() string {
+	return fmt.Sprintf("%s", e.value)
+}
+
+func Flow1() {
+	rand.Seed(time.Now().UnixNano())
+	var x = rand.Uint64() & ((1 << uint(24)) - 1)
+	zeroes := bits.TrailingZeros64(x)
+	fmt.Println(x, zeroes)
+	for i := 0; i < 5; i++ {
+		myList := skiplist.New()
+		fmt.Println(myList)
+
+		reader := bufio.NewReaderSize(os.Stdin, 16*1024*1024)
+		listLen, err := strconv.ParseInt(strings.TrimSpace(readLine(reader)), 10, 32)
+		if err != nil {
+			fmt.Println("[parseInt] integer transform failed")
+		}
+
+		fmt.Println(myList.String())
+		for i := 1; i < int(listLen)+1; i++ {
+			//myList.Insert(Element{float64(i*100-1), "ELEM"+ strconv.Itoa(i*100-1)})
+			//myList.Insert(Element{float64(i*100+1), "ELEM"+ strconv.Itoa(i*100+1)})
+			myList.Insert(IntElement(i * 1000))
+			//myList.Insert(Element{float64(i*100-1), "elem"+ strconv.Itoa(i*100-1)})
+			//myList.Insert(Element{float64(i*100+1), "elem"+ strconv.Itoa(i*100+1)})
+			fmt.Println(myList.String())
+		}
+		fmt.Println(myList.GetNodeCount())
+		fmt.Println(myList.GetSmallestNode())
+		fmt.Println(myList.GetLargestNode())
+		fmt.Println(myList)
+	}
+}
+
+func FLow2() {
+	for i := 0; i < 5; i++ {
+		reader := bufio.NewReaderSize(os.Stdin, 16*1024*1024)
+		eps, err := strconv.ParseInt(strings.TrimSpace(readLine(reader)), 10, 32)
+
+		myList := skiplist.NewEps(float64(eps))
+		fmt.Println(myList)
+
+		if err != nil {
+			fmt.Println("[parseInt] integer transform failed")
+		}
+
+		fmt.Println(myList.String())
+		for i := 1; i < 6; i++ {
+			myList.Insert(IntElement(i * 1000))
+			myList.Insert(IntElement(i * 1000))
+			myList.Insert(IntElement(i * 1000))
+			myList.Insert(IntElement(i * 1000))
+			myList.Insert(IntElement(i * 1000))
+			fmt.Println(myList.String())
+		}
+		for i := 1; i < 6; i++ {
+			fmt.Println(myList.Find(IntElement(i * 1000)))
+		}
+	}
+}
+func readLine(reader *bufio.Reader) string {
+	str, _, err := reader.ReadLine()
+	if err == io.EOF {
+		return ""
+	}
+
+	return strings.TrimRight(string(str), "\r\n")
+}
